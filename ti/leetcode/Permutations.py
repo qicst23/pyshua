c20934f443d43837405a09e7774416448fe25ba8
Junjie Bai <baidingding7@gmail.com>
2013-11-27 10:21:09 +0800

    Permutations

---
+++ ti/leetcode/Permutations.py
@@ -0,0 +1,33 @@
+from ti.leetcode.LeetcodeProblem import LeetcodeProblem
+
+
+class Permutations(LeetcodeProblem):
+    def solve(self, num):
+        return self.permutation(num, len(num), 0)
+
+    def permutation(self, num, n, i):
+        if i == n:
+            return [[]]
+        else:
+            e = [num[i]]
+            res = []
+            for p in self.permutation(num, n, i + 1):
+                res += [p[:j] + e + p[j:] for j in xrange(n - i)]
+            return res
+
+    def verify(self, input, s1, s2):
+        s1Set = set([tuple(l) for l in s1])
+        if len(s1) != len(s1Set):
+            return False
+        return s1Set == set([tuple(l) for l in s2])
+
+    def input(self):
+        from Parser import parseIntArray
+        return parseIntArray(open(self.inputPath))
+
+    def output(self):
+        from Parser import parseIntArrayArrays
+        for o in parseIntArrayArrays(open(self.outputPath)):
+            yield o[0]
+
+problem = Permutations
