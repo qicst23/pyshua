f9ba31016ca24f7e30353d38ffa2c08b217b1ec2
Junjie Bai <baidingding7@gmail.com>
2013-12-01 10:08:57 +0100

    BinaryTreeMaximumPathSum

--- ti/leetcode/BinaryTreeMaximumPathSum.py
+++ ti/leetcode/BinaryTreeMaximumPathSum.py
@@ -3,7 +3,30 @@ from ti.leetcode.LeetcodeProblem import LeetcodeProblem
 
 class BinaryTreeMaximumPathSum(LeetcodeProblem):
     def solve(self, root):
-        return 0
+        return self.maxSum(root)[1]
+
+    def maxSum(self, node):
+        if not node:
+            return 0, 0
+        else:
+            leftOneSide, left = self.maxSum(node.left)
+            rightOndeSide, right = self.maxSum(node.right)
+            oneSide = max(
+                node.val,
+                node.val + leftOneSide,
+                node.val + rightOndeSide
+            )
+            res = max(
+                oneSide,
+                node.val + leftOneSide + rightOndeSide
+            )
+
+            if node.left:
+                res = max(res, left)
+            if node.right:
+                res = max(res, right)
+
+            return oneSide, res
 
     def verify(self, input, s1, s2):
         return s1 == s2
