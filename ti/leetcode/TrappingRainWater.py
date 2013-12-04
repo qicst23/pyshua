c68c675df093a19f72817e948b716c0364c28574
Junjie Bai <baidingding7@gmail.com>
2013-12-04 23:01:09 +0100

    TrappingRainWater

---
+++ ti/leetcode/TrappingRainWater.py
@@ -0,0 +1,40 @@
+from ti.leetcode.LeetcodeProblem import LeetcodeProblem
+
+
+class TrappingRainWater(LeetcodeProblem):
+    def solve(self, a):
+        n = len(a)
+        if n == 0:
+            return 0
+
+        m = 0
+        left = [0] * n
+        for i in xrange(n):
+            if a[i] > m:
+                m = a[i]
+            left[i] = m
+
+        m = 0
+        right = 0
+        res = 0
+        for i in xrange(n - 1, -1, -1):
+            if a[i] > m:
+                m = a[i]
+            right = m
+            res += min(left[i], right) - a[i]
+
+        return res
+
+    def verify(self, input, s1, s2):
+        return s1 == s2
+
+    def input(self):
+        from Parser import parseIntArray
+        return parseIntArray(open(self.inputPath))
+
+    def output(self):
+        from Parser import parseOneInt
+        for o in parseOneInt(open(self.outputPath)):
+            yield o[0]
+
+problem = TrappingRainWater
