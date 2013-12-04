d52c922698f19b41ac410fc441caf932a47657d5
Junjie Bai <baidingding7@gmail.com>
2013-12-04 19:55:22 +0100

    BinaryTree Level Order Traversal

---
+++ ti/leetcode/BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,35 @@
+from ti.leetcode.LeetcodeProblem import LeetcodeProblem
+
+
+class BinaryTreeLevelOrderTraversal(LeetcodeProblem):
+    def solve(self, root):
+        if not root:
+            return []
+
+        nodes = []
+        lastLevel = [root]
+        while lastLevel:
+            nodes.append(lastLevel)
+            nextLevel = []
+            for node in lastLevel:
+                if node.left:
+                    nextLevel.append(node.left)
+                if node.right:
+                    nextLevel.append(node.right)
+            lastLevel = nextLevel
+
+        return [map(lambda n: n.val, level) for level in nodes]
+
+    def verify(self, input, s1, s2):
+        return s1 == s2
+
+    def input(self):
+        from Parser import parseBinaryTree
+        return parseBinaryTree(open(self.inputPath))
+
+    def output(self):
+        from Parser import parseIntArrayArrays
+        for o in parseIntArrayArrays(open(self.outputPath)):
+            yield o[0]
+
+problem = BinaryTreeLevelOrderTraversal
