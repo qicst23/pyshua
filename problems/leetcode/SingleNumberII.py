ef9e4369520f49acea39fd87d8765f63d48b70e7
Junjie Bai <baidingding7@gmail.com>
2013-12-12 00:38:19 +0100

    SingleNumberII

--- problems/leetcode/SingleNumberII.py
+++ problems/leetcode/SingleNumberII.py
@@ -3,7 +3,28 @@ from problems.leetcode.LeetcodeProblem import LeetcodeProblem
 
 class SingleNumberII(LeetcodeProblem):
     def solve(self, a):
-        return min(minSum)
+        record = [0] * 32
+        sign = 0
+        for n in a:
+            if n < 0:
+                sign += 1
+                n = -n
+            i = 0
+            while n:
+                if n & 1:
+                    record[i] += 1
+                i += 1
+                n >>= 1
+            for i in xrange(32):
+                record[i] %= 3
+            sign %= 3
+
+        res = 0
+        m = 1
+        for b in record:
+            res += b * m
+            m *= 2
+        return -res if sign else res
 
     def verify(self, input, s1, s2):
         return s1 == s2
