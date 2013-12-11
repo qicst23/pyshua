e7be09856f5d6ba2814bafb32857e24a81f6c4e7
Junjie Bai <baidingding7@gmail.com>
2013-12-12 00:47:49 +0100

    SearchinRotatedSortedArrayII

---
+++ problems/leetcode/SearchinRotatedSortedArrayII.py
@@ -0,0 +1,23 @@
+from problems.leetcode.LeetcodeProblem import LeetcodeProblem
+
+
+class SearchinRotatedSortedArrayII(LeetcodeProblem):
+    def solve(self, a, target):
+        for n in a:
+            if n == target:
+                return True
+        return False
+
+    def verify(self, input, s1, s2):
+        return s1 == s2
+
+    def input(self):
+        from Parser import parseArrayAndInt
+        return parseArrayAndInt(open(self.inputPath))
+
+    def output(self):
+        from Parser import parseBoolean
+        for o in parseBoolean(open(self.outputPath)):
+            yield o[0]
+
+problem = SearchinRotatedSortedArrayII
