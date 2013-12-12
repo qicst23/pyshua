bf4652f5cfd4b25e7e778a10c3a73b558f6abae1
Junjie Bai <baidingding7@gmail.com>
2013-12-12 01:10:58 +0100

    SearchforaRange

---
+++ problems/leetcode/SearchforaRange.py
@@ -0,0 +1,48 @@
+from problems.leetcode.LeetcodeProblem import LeetcodeProblem
+
+
+class SearchforaRange(LeetcodeProblem):
+    def solve(self, a, target):
+        n = len(a)
+        low = 0
+        high = n - 1
+        left = -1
+        while low <= high:
+            middle = low + (high - low) / 2
+            if a[middle] == target and (middle == 0 or a[middle - 1] < target):
+                left = middle
+                break
+            elif a[middle] < target:
+                low = middle + 1
+            else:
+                high = middle - 1
+
+        low = 0
+        high = n - 1
+        right = -1
+        while low <= high:
+            middle = low + (high - low) / 2
+            if a[middle] == target and (
+                middle == n - 1 or a[middle + 1] > target
+            ):
+                right = middle
+                break
+            elif a[middle] <= target:
+                low = middle + 1
+            else:
+                high = middle - 1
+        return [left, right]
+
+    def verify(self, input, s1, s2):
+        return s1 == s2
+
+    def input(self):
+        from Parser import parseArrayAndInt
+        return parseArrayAndInt(open(self.inputPath))
+
+    def output(self):
+        from Parser import parseIntArray
+        for o in parseIntArray(open(self.outputPath)):
+            yield o[0]
+
+problem = SearchforaRange
