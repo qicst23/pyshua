fdcb0c0807a5d3d261e67ec2a2d5995a58a0d761
Junjie Bai <baidingding7@gmail.com>
2013-12-12 10:22:17 +0100

    WordLadder

---
+++ problems/leetcode/WordLadder.py
@@ -0,0 +1,38 @@
+from problems.leetcode.LeetcodeProblem import LeetcodeProblem
+
+
+class WordLadder(LeetcodeProblem):
+    def solve(self, start, end, dSet):
+        from string import lowercase
+        dSet.add(end)
+        res = 0
+        cur = [start]
+        while cur:
+            res += 1
+            next = []
+            for node in cur:
+                if node == end:
+                    return res
+                for i in xrange(len(node)):
+                    for c in lowercase:
+                        newString = node[:i] + c + node[i + 1:]
+                        if newString in dSet:
+                            next.append(newString)
+                            dSet.remove(newString)
+            cur = next
+        return 0
+
+    def verify(self, input, s1, s2):
+        return s1 == s2
+
+    def input(self):
+        from Parser import parseTwoStringAndStringArray
+        for o in parseTwoStringAndStringArray(open(self.inputPath)):
+            yield o[0], o[1], set(o[2])
+
+    def output(self):
+        from Parser import parseOneInt
+        for o in parseOneInt(open(self.outputPath)):
+            yield o[0]
+
+problem = WordLadder
